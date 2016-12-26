209bb5933996d2e1b155605151c05c00c5711a65
Srivathsan Madhavan <m.srivathsan@gmail.com>
2016-12-26 20:06:03 +0530

    more skeletons

---
+++ Cpp-11/Fsm.cpp
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2016 Srivathsan Madhavan <m_dot_srivathsan_at_gmail_dot_com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* 
+ * File:   Fsm.cpp
+ * Author: Srivathsan Madhavan <m_dot_srivathsan_at_gmail_dot_com>
+ * 
+ * Created on 26 December, 2016, 8:00 PM IST (GMT+05:30)
+ */
+
+#include "Fsm.h"
+
+Fsm::Fsm() {
+}
+
+Fsm::Fsm(const Fsm& orig) {
+}
+
+Fsm::~Fsm() {
+}
+
---
+++ Cpp-11/Fsm.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2016 Srivathsan Madhavan <m_dot_srivathsan_at_gmail_dot_com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* 
+ * File:   Fsm.h
+ * Author: Srivathsan Madhavan <m_dot_srivathsan_at_gmail_dot_com>
+ *
+ * Created on 26 December, 2016, 8:00 PM
+ */
+
+#ifndef FSM_H
+#define FSM_H
+
+class Fsm {
+public:
+    Fsm();
+    Fsm(const Fsm& orig);
+    virtual ~Fsm();
+private:
+
+};
+
+#endif /* FSM_H */
+
